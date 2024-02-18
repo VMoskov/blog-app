e8158cbaada92282b73c535d371d74ede29b217b
VMoskov <vedran.moskov@fer.hr>
2024-02-18 19:30:43 +0100

    added click events

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log('hello, ninjas', e);
+    }
+
+    const handleClickAgain = (name) => {
+        console.log('hello ' + name);
+    }
+
     return (
         <div className='home'>
             <h2>Home Page</h2>
+            <button onClick={ handleClick }>Click me!</button>
+            <button onClick={ () => handleClickAgain('Vedranousky') }>Click me again!</button>
         </div>
     )
 }
